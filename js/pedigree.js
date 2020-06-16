2996b333207063aaaa7171d848c47dabc6a43c44
dominic-lawrence-bermudez <56672015+dominic-lawrence-bermudez@users.noreply.github.com>
2020-06-17 04:11:11 +0800

    added functions for generating a full family starting from 2 people

--- js/pedigree.js
+++ js/pedigree.js
@@ -1,2 +1,33 @@
+//--- ----- Generating a Family
 
+function generateFamily() {
+	var Grandfather = new Person(1, "male");
+	var Grandmother = new Person(1, "female");
+	
+	Grandfather.AutosomalGenes = Person.generateRandomGenes();
+	Grandmother.AutosomalGenes = Person.generateRandomGenes();
+	
+	Person.marry(Grandfather, Grandmother);
+	Person.haveChildren(Grandfather, Grandmother);
+	
+	for (let child of Grandfather.Children) {
+		continueFamily(child);
+	}
+	
+	return Grandfather;
+}
 
+// recursive magic
+function continueFamily(person) {
+	if (person.Generation <= MaxGeneration) {
+		var personIsMarried = person.tryToMarry();
+		
+		if (personIsMarried) {
+			Person.haveChildren(person, person.Partner);
+			
+			for (let child of person.Children) {
+				continueFamily(child);
+			}
+		}
+	}
+}
