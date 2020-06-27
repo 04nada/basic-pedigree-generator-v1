95b5411885a9938a60a8c4e13548c195d3f289ec
dominic-lawrence-bermudez <56672015+dominic-lawrence-bermudez@users.noreply.github.com>
2020-06-27 23:53:50 +0800

    fixed display functions, integrated code with CSS designs

--- js/pedigree.js
+++ js/pedigree.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
 	clearSVG();
-	resetTextDivs();
+	resetContent();
+	
+	id_pedigreeSVG.style.height = 2 * MAX_GENERATION * SYMBOL_LENGTH_px;
+
+	alert(SYMBOL_LENGTH_px);
 });
 
-//--- -----
+// resize SVG when device is rotated
+window.addEventListener("orientationchange", function(){
+	setTimeout(function() {
+		SYMBOL_LENGTH_px = parseFloat(document.getCSSPropertyById("id-pedigreeSVG", "width")) / 28.0;
+		
+		id_pedigreeSVG.style.height = 2 * MAX_GENERATION * SYMBOL_LENGTH_px;
+	}, 1000);
+});
+
+//--- ----- Autosomal Traits
 
 const eyeColor = new AutosomalTrait("Blue eye color", 'B', 'b', "brown eyes", "blue eyes", "recessive");
 eyeColor.setDescription(
@@ -20,30 +33,38 @@ tritanopia.setDescription(
 	"blue-yellow colorblindness"
 );
 
+//---  ----- HTML Elements by ID
+
+const id_traitExpressionDiv = document.getElementById("id-traitExpressionDiv");
+	const id_traitExpressionForm = document.getElementById("id-traitExpressionForm");
+		const id_submitTraitExpression = document.getElementById("id-submitTraitExpression");
+	const id_traitExpressionOutput = document.getElementById("id-traitExpressionOutput");
+
+const id_traitAnalysisDiv = document.getElementById("id-traitAnalysisDiv");
+	const id_traitInfo = document.getElementById("id-traitInfo");
+		const id_traitName = document.getElementById("id-traitName");
+		const id_dominantTrait = document.getElementById("id-dominantTrait");
+		const id_recessiveTrait = document.getElementById("id-recessiveTrait");
+
 //---
 
-const id_traitName = document.getElementById("id-traitName");
-const id_dominantTrait = document.getElementById("id-dominantTrait");
-const id_recessiveTrait = document.getElementById("id-recessiveTrait");
-
-//---
-
-var activeTraitName, activeTrait, activePhenotype;
+var activeTraitName, activeTrait;
 var ped1, pedGF, pedGM;
 
 function generatePedigree() {
+	console.clear();
+	resetContent();
+	
+	//--- Pedigree Generation
+	
+	// choose a random trait for the pedigree
 	activeTraitName = Object.keys(DefinedAutosomalTraits).getRandomElement();
 	activeTrait = DefinedAutosomalTraits[activeTraitName];
-
-	//---
-	
-	console.clear();
 	
 	// keep generating new pedigrees until a sizable and solvable one is obtained
 	while (true) {
 		clearSVG();
-		resetTextDivs();
-		
+
 		ped1 = new Pedigree(activeTrait);
 		pedGF = ped1.Family.Grandfather;
 		pedGM = ped1.Family.Grandmother;
@@ -54,20 +75,23 @@ function generatePedigree() {
 			break;
 	}
 	
-	id_textDiv.style.visibility = "visible";
-	
 	console.log("by Generation: ");
 	console.log(ped1.Family.Generations);
 	
 	console.log("by Genotype: ");
 	console.log(ped1.Family.MembersBySolvableGenotype);
 	
-	//---
+	//--- Display HTML Divs
 	
-	id_traitName.innerHTML = "Trait: " + activeTraitName;
+	id_traitExpressionDiv.style.display = "block";
 	
-	id_dominantTrait.innerHTML = "Dominant: " + activeTrait.DominantPhenotype;
-	id_recessiveTrait.innerHTML = "Recessive: " + activeTrait.RecessivePhenotype;
+		id_submitTraitExpression.disabled = false;
+		id_submitTraitExpression.style.cursor = "pointer";
+		
+		for (let el of id_traitExpressionForm.getAllFormElements()) {
+			el.disabled = false;
+			el.style.cursor = "pointer";
+		}
 }
 
 function clearSVG() {
@@ -78,29 +102,62 @@ function clearSVG() {
 
 //--- -----
 
-const id_textDiv = document.getElementById("id-textDiv");
-
-const id_traitExpressionForm = document.getElementById("id-traitExpressionForm");
-const id_traitExpressionOutput = document.getElementById("id-traitExpressionOutput");
-
-//---
-
-function resetTextDivs() {
-	id_traitExpressionOutput.innerHTML = "";
+function resetContent() {
+	id_traitExpressionDiv.style.display = "none";
 	
-	id_textDiv.style.visibility = "hidden";
+		id_traitExpressionOutput.innerHTML = "";
+		id_traitExpressionOutput.style.display = "none";
+	
+	id_traitAnalysisDiv.style.display = "none";
+		
+		const submit_expression = id_traitExpressionForm.elements["name-traitExpression"];
+		submit_expression.selectedIndex = 0;
+
+		id_traitInfo.style.display = "none";
 }
 
 //---
 
 function submitTraitExpression() {
-	var submit_expression = id_traitExpressionForm.elements["name-traitExpression"];
+	for (let el of id_traitExpressionForm.getAllFormElements()) {
+		el.disabled = true;
+		el.style.cursor = "not-allowed";
+	}
+	
+	//--- -----
+	
+	id_traitExpressionOutput.style.display = "block";
+	
+	//---
+	
+	const submit_expression = id_traitExpressionForm.elements["name-traitExpression"];
 	
 	if (submit_expression.value === ped1.ActiveTrait.Expression) {
-		id_traitExpressionOutput.innerHTML = "Correct: " + ped1.ActiveTrait.TraitName + " is " + ped1.ActiveTrait.Expression + ".";
+		id_traitExpressionOutput.style.backgroundColor = "#BFB";
+		id_traitExpressionOutput.innerHTML = "Correct: The trait is " + ped1.ActiveTrait.Expression + ".";
 	} else {
-		id_traitExpressionOutput.innerHTML = "Wrong: " + ped1.ActiveTrait.TraitName + " is " + ped1.ActiveTrait.Expression + ".";
+		id_traitExpressionOutput.style.backgroundColor = "#FBB";
+		id_traitExpressionOutput.innerHTML = "Incorrect: The trait is " + ped1.ActiveTrait.Expression + ".";
 	}
+	
+	//---
+	
+	
+	
+	//--- ----- Display Trait Info
+	
+	id_traitAnalysisDiv.style.display = "block";
+	
+		id_traitInfo.style.display = "block";
+	
+	//---
+	
+	id_traitName.innerHTML = "Trait: " + activeTraitName;
+	
+	id_dominantTrait.innerHTML = "Dominant (" + activeTrait.DominantAllele + "): " + activeTrait.DominantPhenotype;
+	id_recessiveTrait.innerHTML = "Recessive (" + activeTrait.RecessiveAllele + "): " + activeTrait.RecessivePhenotype;
+	
+	//--- -----
 	
 	return false;
 }
